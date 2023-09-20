db7c26cadfac6ecaac59974c00832fee5c80e8e3
John Coene <jcoenep@gmail.com>
2023-09-20 10:40:04 +0200

    feat: init recursive search

--- srcjs/tree.js
+++ srcjs/tree.js
@@ -1,47 +1,126 @@
 jQuery.fn.extend({
   treeInput: function () {
-    let id = $(this).attr("id");
+    addSearchBasic(this);
+    // searchRecurse(this);
+  },
+});
 
-    $(`#${id}-query`).on("keydown", (event) => {
-      if (event.which != 13) {
-        return;
-      }
+const addSearchBasic = (parent) => {
+  let id = $(parent).attr("id");
 
-      $(`#${id}-search`).trigger("click");
-    });
+  $(`#${id}-query`).on("keydown", (event) => {
+    if (event.which != 13) {
+      return;
+    }
 
-    $(`#${id}-search`).on("click", (event) => {
-      let query = $(`#${id}-query`).val().toLowerCase();
+    $(`#${id}-search`).trigger("click");
+  });
 
-      if (query == "") {
-        $(this).find(".treejs-node").each((i, el) => {
-          if (!$(el).hasClass("treejs-node__close")) {
-            $(el).find(".treejs-switcher").trigger("click");
-          }
+  $(`#${id}-search`).on("click", (event) => {
+    let query = $(`#${id}-query`).val().toLowerCase();
 
-          $(el).show();
-        });
-        return;
-      }
-
-      $(this).find(".treejs-node").each((i, el) => {
-        if ($(el).hasClass("treejs-node__close")) {
+    if (query == "") {
+      $(parent).find(".treejs-node").each((i, el) => {
+        if (!$(el).hasClass("treejs-node__close")) {
           $(el).find(".treejs-switcher").trigger("click");
         }
 
-        if ($(el).find(".treejs-node").length > 0) {
-          return;
-        }
-
-        const label = $(el).find(".treejs-label").text().toLowerCase();
-
-        if (label.includes(query)) {
-          $(el).show();
-          return;
-        }
-
-        $(el).hide();
+        $(el).show();
       });
+      return;
+    }
+
+    $(parent).find(".treejs-node").each((i, el) => {
+      if ($(el).hasClass("treejs-node__close")) {
+        $(el).find(".treejs-switcher").trigger("click");
+      }
+
+      if ($(el).find(".treejs-node").length > 0) {
+        return;
+      }
+
+      const label = $(el).find(".treejs-label").text().toLowerCase();
+
+      if (label.includes(query)) {
+        $(el).show();
+        return;
+      }
+
+      $(el).hide();
     });
-  },
-});
+  });
+};
+
+const searchRecurse = (parent) => {
+  let id = $(parent).attr("id");
+
+  $(`#${id}-query`).on("keydown", (event) => {
+    if (event.which != 13) {
+      return;
+    }
+
+    $(`#${id}-search`).trigger("click");
+  });
+
+  $(`#${id}-search`).on("click", (event) => {
+    let query = $(`#${id}-query`).val().toLowerCase();
+
+    if (query == "") {
+      $(parent).find(".treejs-nodes").each((i, el) => {
+        if (!$(el).hasClass("treejs-node__close")) {
+          $(el).find(".treejs-switcher").trigger("click");
+        }
+
+        $(el).show();
+      });
+      return;
+    }
+
+    hideChildren(parent, query);
+  });
+};
+
+const hideChildren = (el, query) => {
+  const children = $(el).find(".treejs-nodes");
+
+  // it has children we recurse
+  if (children.length > 0) {
+    return hideChildren(children, query);
+  }
+
+  $(el).find(".treejs-node").each((i, node) => {
+    const label = $(node).find(".treejs-label").text().toLowerCase();
+
+    if (!label.includes(query)) {
+      $(node).hide();
+      return;
+    }
+
+    $(node).show();
+  });
+
+  hideParents($(el).closest(".treejs-node"), query);
+};
+
+const hideParents = (parent, query) => {
+  console.log(parent);
+  let anyVisible = 0;
+
+  $(parent).find(".treejs-node").each((i, node) => {
+    const isVisible = $(node).is(":visible");
+
+    if (isVisible) {
+      anyVisible++;
+    }
+  });
+
+  if (!anyVisible) {
+    $(parent).hide();
+  }
+
+  if ($(parent).closest(".treejs-nodes").length == 0) {
+    return;
+  }
+
+  hideParents($(parent).closest(".treejs-nodes"), query);
+};
